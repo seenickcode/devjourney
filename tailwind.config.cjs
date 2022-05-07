4652143cc0e50b6da8b8f2d6dd7aaa769a15cf5d
Nick Manning <hello@manninglabs.com>
2022-05-07 17:39:49 -0400

    Blank homepage.

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      // sans: ["source_sans_proregular", "Arial"],
+      // serif: ["oswaldmedium", "Courier"],
+      // serifregular: ["oswald_regularregular", "Courier"],
+    },
+  },
+  plugins: [],
+};
